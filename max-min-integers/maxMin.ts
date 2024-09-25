507751943e1cf528f79aea7a11bd60b9880a4eed
arturmartins <arturmartinssilva078@gmail.com>
2024-09-25 18:23:49 -0300

    style: correção da formatação

--- max-min-integers/maxMin.ts
+++ max-min-integers/maxMin.ts
@@ -1,8 +1,8 @@
-// Recebe uma lista de inteiros como entrada e retornam 
+// Recebe uma lista de inteiros como entrada e retornam
 // o maior e o menor número dessa lista, respectivamente.
 
-const min = (list: number[]): number => Math.min(...list)
-const max = (list: number[]): number => Math.max(...list)
+const min = (list: number[]): number => Math.min(...list);
+const max = (list: number[]): number => Math.max(...list);
 
-console.log(min([2, 4, 5, -98]))
-console.log(max([34, 2, 5, 9]))
+console.log(min([2, 4, 5, -98]));
+console.log(max([34, 2, 5, 9]));
