3ebbbeab41657d6f8c69952344d14457667290ac
arturmartins <arturmartinssilva078@gmail.com>
2024-09-25 18:24:51 -0300

    style: correção da formatação

--- check-Even-or-Odd/evenOdd.ts
+++ check-Even-or-Odd/evenOdd.ts
@@ -1,12 +1,12 @@
 // Implemente uma função que recebe um número e verifica se ele é par ou ímpar.
-// Caso o valor passado não seja um número, a função deve retornar null. 
+// Caso o valor passado não seja um número, a função deve retornar null.
 // Use o operador módulo (%) para realizar a verificação.
 
-const checkEvenOdd = (numero:number) => {
-    if(typeof numero != "number") return null;
-    if(numero % 2 === 0) return "Par"
-    return "Ímpar"
-}
+const checkEvenOdd = (numero: number) => {
+  if (typeof numero != "number") return null;
+  if (numero % 2 === 0) return "Par";
+  return "Ímpar";
+};
 
 console.log(checkEvenOdd(4)); //Par
 console.log(checkEvenOdd(7)); //Ímpar
