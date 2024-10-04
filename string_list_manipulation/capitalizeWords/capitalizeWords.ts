73fc0369c35d745047534c586cb7610d1322a264
arturmartins <arturmartinssilva078@gmail.com>
2024-10-04 11:54:05 -0300

    refactor: movidos para a pasta string_list_manipulation e modificação na variável "frase"

---
+++ string_list_manipulation/capitalizeWords/capitalizeWords.ts
@@ -0,0 +1,24 @@
+/*
+Desafio: Adicionar um método à classe String que converte uma string em "Jaden Case".
+Regras:
+1. O método `toJadenCase` deve capitalizar a primeira letra de cada palavra em uma string.
+2. Deve ser adicionado ao protótipo da classe `String`.
+3. A interface `String` deve ser modificada para incluir o método `toJadenCase`.
+
+Exemplo:
+Input: "how can mirrors be real if our eyes aren't real"
+Output: "How Can Mirrors Be Real If Our Eyes Aren't Real"
+*/
+
+String.prototype.toJadenCase = function (): string {
+  return this.split(" ") // separa as palavras a partir de um espaço
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+interface String {
+  toJadenCase(): string;
+}
+
+const frase = "how can mirrors be real if our eyes aren't real";
+console.log(frase.toJadenCase()); // Output: "How Can Mirrors Be Real If Our Eyes Aren't Real"
